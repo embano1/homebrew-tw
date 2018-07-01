25dbf1fcf8a9564f57f9ec146b84a538f2944bc7
goreleaserbot <embano1@live.com>
2018-07-01 23:03:02 +0200

    Brew formula update for tw version v1.0

--- tw.rb
+++ tw.rb
@@ -1,9 +1,9 @@
 class Tw < Formula
   desc "'tw' is a very basic Twitter client to query your Likes, and do some powerful JSON filtering."
   homepage "https://github.com/embano1/tw"
-  url "https://github.com/embano1/tw/releases/download/v0.9.2/tw_0.9.2_darwin_amd64.tar.gz"
-  version "0.9.2"
-  sha256 "84a4670b6621125063776bdfdb48879a160621d1a190c7f43a6ac3bc4e085128"
+  url "https://github.com/embano1/tw/releases/download/v1.0/tw_1.0_darwin_amd64.tar.gz"
+  version "1.0"
+  sha256 "927a8b8a19b93fad8e185559ca5aa771e6b53c8f705c25ccf580ce7b57d9a0ed"
 
   def install
     bin.install "tw"
